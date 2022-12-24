ee2e0c499675a5d75041e0baa92d808e8f8f91d9
Myk Taylor <myk002@yahoo.com>
2022-12-23 19:37:33 -0800

    fix calling conventions and names

--- DFHooks.cpp
+++ DFHooks.cpp
@@ -1,4 +1,4 @@
-#include <DFHooks.h>
+#include "DFHooks.h"
 
 #include <string>
 
@@ -16,26 +16,26 @@
 const std::string g_lib_name = "dfhack";
 void *g_lib_handle = nullptr;
 
-typedef void (__cdecl *dfhack_init_fn)();
+typedef void (*dfhack_init_fn)();
 dfhack_init_fn g_dfhack_init = nullptr;
 
-typedef void (__cdecl *dfhack_shutdown_fn)();
+typedef void (*dfhack_shutdown_fn)();
 dfhack_shutdown_fn g_dfhack_shutdown = nullptr;
 
-typedef void (__cdecl *dfhack_tick_fn)();
+typedef void (*dfhack_tick_fn)();
 dfhack_tick_fn g_dfhack_tick = nullptr;
 
-typedef void (__cdecl *dfhack_prerender_fn)();
+typedef void (*dfhack_prerender_fn)();
 dfhack_prerender_fn g_dfhack_prerender = nullptr;
 
-typedef bool (__cdecl *dfhack_sdl_event_fn)(SDL::Event* event);
+typedef bool (*dfhack_sdl_event_fn)(SDL::Event* event);
 dfhack_sdl_event_fn g_dfhack_sdl_event = nullptr;
 
 static void * open_library(const std::string &lib_name) {
 #if _WIN32
     return LoadLibrary((lib_name + ".dll").c_str());
 #else
-    return dlopen((lib_name + lib_ext).c_str(), RTLD_LAZY);
+    return dlopen(("./lib" + lib_name + lib_ext).c_str(), RTLD_LAZY);
 #endif
 }
 
@@ -64,9 +64,9 @@ static void *load_sym(void *handle, const char *sym) {
 static void init_fns(void *handle) {
     g_dfhack_init = (dfhack_init_fn)load_sym(handle, "dfhack_init");
     g_dfhack_shutdown = (dfhack_shutdown_fn)load_sym(handle, "dfhack_shutdown");
-    g_dfhack_tick = (dfhack_tick_fn)load_sym(handle, "dfhack_init");
-    g_dfhack_prerender = (dfhack_prerender_fn)load_sym(handle, "dfhack_init");
-    g_dfhack_sdl_event = (dfhack_sdl_event_fn)load_sym(handle, "dfhack_init");
+    g_dfhack_tick = (dfhack_tick_fn)load_sym(handle, "dfhack_tick");
+    g_dfhack_prerender = (dfhack_prerender_fn)load_sym(handle, "dfhack_prerender");
+    g_dfhack_sdl_event = (dfhack_sdl_event_fn)load_sym(handle, "dfhack_sdl_event");
 }
 
 void hooks_init(void) {
