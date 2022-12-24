9284a22bd1b68a29d62e754fd29ad7064fa8dd3f
Myk Taylor <myk002@yahoo.com>
2022-12-24 06:06:14 -0800

    make dfhooks DFHack-agnostic

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hooks
-Shim code compiled into DF that loads and calls DFHack
+Shim code compiled into DF that loads and calls a "dfhooks" library. This
+library should be located in the root of the DF game directory.
--- dfhooks.cpp
+++ dfhooks.cpp
@@ -13,29 +13,29 @@
 #   endif
 #endif
 
-const std::string g_lib_name = "dfhack";
+const std::string g_lib_name = "dfhooks";
 void *g_lib_handle = nullptr;
 
-typedef void (*dfhack_init_fn)();
-dfhack_init_fn g_dfhack_init = nullptr;
+typedef void (*dfhooks_init_fn)();
+dfhooks_init_fn g_dfhooks_init = nullptr;
 
-typedef void (*dfhack_shutdown_fn)();
-dfhack_shutdown_fn g_dfhack_shutdown = nullptr;
+typedef void (*dfhooks_shutdown_fn)();
+dfhooks_shutdown_fn g_dfhooks_shutdown = nullptr;
 
-typedef void (*dfhack_update_fn)();
-dfhack_update_fn g_dfhack_update = nullptr;
+typedef void (*dfhooks_update_fn)();
+dfhooks_update_fn g_dfhooks_update = nullptr;
 
-typedef void (*dfhack_prerender_fn)();
-dfhack_prerender_fn g_dfhack_prerender = nullptr;
+typedef void (*dfhooks_prerender_fn)();
+dfhooks_prerender_fn g_dfhooks_prerender = nullptr;
 
-typedef bool (*dfhack_sdl_event_fn)(SDL::Event* event);
-dfhack_sdl_event_fn g_dfhack_sdl_event = nullptr;
+typedef bool (*dfhooks_sdl_event_fn)(SDL::Event* event);
+dfhooks_sdl_event_fn g_dfhooks_sdl_event = nullptr;
 
 static void * open_library(const std::string &lib_name) {
 #if _WIN32
     return LoadLibrary((lib_name + ".dll").c_str());
 #else
-    return dlopen(("hack/lib" + lib_name + lib_ext).c_str(), RTLD_LAZY);
+    return dlopen(("lib" + lib_name + lib_ext).c_str(), RTLD_LAZY);
 #endif
 }
 
@@ -62,24 +62,24 @@ static void *load_sym(void *handle, const char *sym) {
 }
 
 static void init_fns(void *handle) {
-    g_dfhack_init = (dfhack_init_fn)load_sym(handle, "dfhack_init");
-    g_dfhack_shutdown = (dfhack_shutdown_fn)load_sym(handle, "dfhack_shutdown");
-    g_dfhack_update = (dfhack_update_fn)load_sym(handle, "dfhack_update");
-    g_dfhack_prerender = (dfhack_prerender_fn)load_sym(handle, "dfhack_prerender");
-    g_dfhack_sdl_event = (dfhack_sdl_event_fn)load_sym(handle, "dfhack_sdl_event");
+    g_dfhooks_init = (dfhooks_init_fn)load_sym(handle, "dfhooks_init");
+    g_dfhooks_shutdown = (dfhooks_shutdown_fn)load_sym(handle, "dfhooks_shutdown");
+    g_dfhooks_update = (dfhooks_update_fn)load_sym(handle, "dfhooks_update");
+    g_dfhooks_prerender = (dfhooks_prerender_fn)load_sym(handle, "dfhooks_prerender");
+    g_dfhooks_sdl_event = (dfhooks_sdl_event_fn)load_sym(handle, "dfhooks_sdl_event");
 }
 
 void hooks_init() {
     g_lib_handle = open_library(g_lib_name);
     init_fns(g_lib_handle);
 
-    if (g_dfhack_init)
-        g_dfhack_init();
+    if (g_dfhooks_init)
+        g_dfhooks_init();
 }
 
 void hooks_shutdown() {
-    if (g_dfhack_shutdown)
-        g_dfhack_shutdown();
+    if (g_dfhooks_shutdown)
+        g_dfhooks_shutdown();
 
     init_fns(nullptr);
     close_lib(g_lib_handle);
@@ -87,17 +87,17 @@ void hooks_shutdown() {
 }
 
 void hooks_update() {
-    if (g_dfhack_update)
-        g_dfhack_update();
+    if (g_dfhooks_update)
+        g_dfhooks_update();
 }
 
 void hooks_prerender() {
-    if (g_dfhack_prerender)
-        g_dfhack_prerender();
+    if (g_dfhooks_prerender)
+        g_dfhooks_prerender();
 }
 
 bool hooks_sdl_event(SDL::Event* event) {
-    if (g_dfhack_sdl_event)
-        return g_dfhack_sdl_event(event);
+    if (g_dfhooks_sdl_event)
+        return g_dfhooks_sdl_event(event);
     return false;
 }
